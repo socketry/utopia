cb2c81384dab06594178536eae2f572194adce49
Samuel Williams <samuel@Nekomimi.local>
2010-02-05 15:50:06 +1300

    Added active_support extension. This is used for date/time/datetime conversions and comparison functions. Made Rack extension optional.

--- lib/utopia/extensions.rb
+++ lib/utopia/extensions.rb
@@ -1,4 +1,21 @@
 
+require 'active_support'
+
+class Date
+	alias_method :old_cmp, :<=>
+	
+	def <=> (other)
+		# Comparing a Date with something that has a time component truncates the time
+		# component, thus we need to check if the other object has a more exact comparison
+		# function.
+		if other.respond_to?(:hour)
+			return (other <=> self) * -1
+		else
+			old_cmp(other)
+		end
+	end	
+end
+
 class String
 	HTML_ESCAPE = {"&" => "&amp;", "<" => "&lt;", ">" => "&gt;", "\"" => "&quot;"}
 	HTML_ESCAPE_PATTERN = Regexp.new("[" + Regexp.quote(HTML_ESCAPE.keys.join) + "]")
@@ -47,8 +64,10 @@ class Array
 	end
 end
 
-class Rack::Request
-	def self.new(*args)
-		super(*args)
+if defined? Rack
+	class Rack::Request
+		def self.new(*args)
+			super(*args)
+		end
 	end
 end
