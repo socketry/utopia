17f9f22aca3a84e3bb695be251dd590a5663b2df
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2010-03-21 15:42:09 +1300

    Added new env tag for restricting output to certain environments.

---
+++ lib/utopia/tags/env.rb
@@ -0,0 +1,29 @@
+# Copyright (c) 2010 Samuel Williams. Released under the GNU GPLv3.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'utopia/tags'
+
+Utopia::Tags.create("env") do |transaction, state|
+	only = state[:only].split(",").collect(&:to_sym) rescue []
+	
+	$stderr.puts "env: #{only.inspect} -> #{UTOPIA_ENV}"
+	
+	if defined?(UTOPIA_ENV) && only.include?(UTOPIA_ENV)
+		$stderr.puts "env: okay"
+		transaction.parse_xml(state.content)
+	else
+		$stderr.puts "env: ignored"
+	end
+end
