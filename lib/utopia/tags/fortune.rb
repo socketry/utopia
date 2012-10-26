3f2707a405f0d1740d2e6a43daf2ad0f3b553641
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-10-27 04:15:02 +1300

    Moving secondary middleware to utopia-contrib gem.

--- lib/utopia/tags/fortune.rb
+++
@@ -1,9 +0,0 @@
-#	This file is part of the "Utopia Framework" project, and is released under the MIT license.
-#	Copyright 2010 Samuel Williams. All rights reserved.
-#	See <utopia.rb> for licensing details.
-
-require 'utopia/tags'
-
-Utopia::Tags.create("fortune") do |transaction, state|
-	"<pre>#{`fortune`.to_html}</pre>"
-end
