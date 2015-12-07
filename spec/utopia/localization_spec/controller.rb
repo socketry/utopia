0629793fa4c1db887e5678f2d4fa81d45ddadc3b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2015-12-08 02:12:59 +1300

    Missing localization_spec/controller.rb

---
+++ spec/utopia/localization_spec/controller.rb
@@ -0,0 +1,18 @@
+
+on 'all_locales' do |request, path|
+	wrapper = Utopia::Localization[request]
+	
+	succeed! content: wrapper.all_locales.join(',')
+end
+
+on 'default_locale' do |request, path|
+	wrapper = Utopia::Localization[request]
+	
+	succeed! content: wrapper.default_locale
+end
+
+on 'current_locale' do |request, path|
+	wrapper = Utopia::Localization[request]
+	
+	succeed! content: wrapper.current_locale
+end
