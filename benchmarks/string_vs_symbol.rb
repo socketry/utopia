c51c27edb81e6018b3c1d97e65a96af3a5d4c1d8
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-10-08 16:05:22 +1300

    String vs symbol hash lookup benchmark.

---
+++ benchmarks/string_vs_symbol.rb
@@ -0,0 +1,12 @@
+require 'benchmark/ips'
+
+STRING_HASH = { "foo" => "bar" }
+SYMBOL_HASH = { :foo => "bar"  }
+
+Benchmark.ips do |x|
+	x.report("string") { STRING_HASH["foo"] }
+	x.report("symbol") { SYMBOL_HASH[:foo]  }
+	x.report("symbol-from-string") { SYMBOL_HASH["foo".to_sym]  }
+	
+	x.compare!
+end
