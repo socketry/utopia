cefcd5aeb225c2d44c2ef632aaf1986393de99af
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-03-10 12:53:24 +1300

    Spec helper for coverage/testing.

--- Rakefile
+++ Rakefile
@@ -1,13 +1,7 @@
 require "bundler/gem_tasks"
 require "rspec/core/rake_task"
 
-RSpec::Core::RakeTask.new(:spec) do |task|
-	begin
-		require('simplecov/version')
-		task.rspec_opts = %w{--require simplecov} if ENV['COVERAGE']
-	rescue LoadError
-	end
-end
+RSpec::Core::RakeTask.new(:spec)
 
 task :documentation do
 	sh('bundle install')
---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+require "bundler/setup"
+require "rack/freeze"
+
+if ENV['COVERAGE']
+	begin
+		require 'simplecov'
+		
+		SimpleCov.start do
+			add_filter "/spec/"
+		end
+		
+		if ENV['TRAVIS']
+			require 'coveralls'
+			Coveralls.wear!
+		end
+	rescue LoadError
+		warn "Could not load simplecov: #{$!}"
+	end
+end
+
+RSpec.configure do |config|
+	# Enable flags like --only-failures and --next-failure
+	config.example_status_persistence_file_path = ".rspec_status"
+
+	config.expect_with :rspec do |c|
+		c.syntax = :expect
+	end
+end
