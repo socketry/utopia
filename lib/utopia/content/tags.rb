e6745ababdf518c712b48bb37a7bc4ddc6093aea
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-03-21 16:30:48 +1300

    Fix missing tags/library

--- lib/utopia/content/tags.rb
+++ lib/utopia/content/tags.rb
@@ -18,6 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+require_relative 'tags/library'
 require_relative 'tags/deferred_tag'
 require_relative 'tags/node_tag'
 require_relative 'tags/content_tag'
