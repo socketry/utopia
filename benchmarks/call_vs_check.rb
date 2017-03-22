a2d4bba56c60e02a85facc9a49196ccd6ac98aca
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-03-23 01:27:47 +1300

    Compare call vs check and call.

---
+++ benchmarks/call_vs_check.rb
@@ -0,0 +1,36 @@
+require 'benchmark/ips'
+
+class A
+end
+
+class B
+	def self.do
+	end
+end
+
+class C
+	def self.do
+	end
+end
+
+Benchmark.ips do |x|
+	x.report("responds_to? (nothing)") do |times|
+		while (times -= 1) >= 0
+			A.do if A.respond_to?(:do)
+		end
+	end
+	
+	x.report("B (empty method)") do |times|
+		while (times -= 1) >= 0
+			B.do
+		end
+	end
+	
+	x.report("responds_to? (empty method)") do |times|
+		while (times -= 1) >= 0
+			C.do if C.respond_to?(:do)
+		end
+	end
+	
+	x.compare!
+end
