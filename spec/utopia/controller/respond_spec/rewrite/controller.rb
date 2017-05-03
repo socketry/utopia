1671b75603ce9d6df083003565a9d3a636c28d9b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-05-03 13:47:40 +1200

    Show handling of errors using json.

---
+++ spec/utopia/controller/respond_spec/rewrite/controller.rb
@@ -0,0 +1,12 @@
+
+prepend Respond, Rewrite, Actions
+
+respond.with_json
+
+rewrite.extract_prefix id: Integer do |request|
+	fail! :not_found, message: "Could not find record" if @id == 1
+end
+
+on 'show' do
+	succeed! content: {id: @id, foo: 'bar'}
+end
