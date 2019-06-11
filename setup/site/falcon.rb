3d5de2c955b2a673a9636c4bb0b1d24f2d18a8ad
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-06-11 19:40:22 +1200

    Fix default falcon config.

--- setup/site/falcon.rb
+++ setup/site/falcon.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S falcon host
 
-load :rack, :self_signed_tls, :supervisor
+load :rack, :lets_encrypt_tls, :supervisor
 
 hostname = File.basename(__dir__)
-rack hostname, :lets_encrypt
+rack hostname, :lets_encrypt_tls
 
 supervisor
