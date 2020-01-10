b410f4b547714bf14e5cd897a8fe3636623a78f4
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-01-10 15:31:48 +1300

    Use spider for testing entire website.

--- setup/site/spec/website_spec.rb
+++ setup/site/spec/website_spec.rb
@@ -2,14 +2,24 @@
 require_relative 'website_context'
 
 # Learn about best practice specs from http://betterspecs.org
-RSpec.describe "my website" do
-	include_context "website"
+RSpec.describe "website", timeout: 120 do
+	include_context "server"
 	
-	it "should have an accessible front page" do
-		get "/"
+	let(:spider) {Benchmark::HTTP::Spider.new(depth: 128)}
+	let(:statistics) {Benchmark::HTTP::Statistics.new}
+	
+	it "should be responsive" do
+		Async::HTTP::Client.open(endpoint, connection_limit: 8) do |client|
+			spider.fetch(statistics, client, endpoint.url) do |method, uri, response|
+				#if response.failure?
+					Async.logger.error{"#{method} #{uri} -> #{response.status}"}
+				#end
+			end.wait
+		end
 		
-		follow_redirect!
+		statistics.print
 		
-		expect(last_response.status).to be == 200
+		expect(statistics.samples).to be_any
+		expect(statistics.failed).to be_zero
 	end
 end
