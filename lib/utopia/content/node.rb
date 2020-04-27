4c2bb1d6abc29b14863c6d3810d1f61fee8913fd
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-04-27 12:33:22 +1200

    Whitespace.

--- lib/utopia/content/node.rb
+++ lib/utopia/content/node.rb
@@ -33,28 +33,28 @@ module Utopia
 		class Node
 			def initialize(controller, uri_path, request_path, file_path)
 				@controller = controller
-
+				
 				@uri_path = uri_path
 				@request_path = request_path
 				@file_path = file_path
 			end
-
+			
 			attr :request_path
 			attr :uri_path
 			attr :file_path
-
+			
 			def name
 				@uri_path.basename
 			end
-
+			
 			def link
 				return Link.file(uri_path)
 			end
-
+			
 			def lookup_node(path)
 				@controller.lookup_node(parent_path + Path[path])
 			end
-
+			
 			def local_path(path = '.', base = nil)
 				path = Path[path]
 				
@@ -78,7 +78,7 @@ module Utopia
 			def parent_path
 				@uri_path.dirname
 			end
-
+			
 			def links(path = '.', **options, &block)
 				path = uri_path.dirname + Path[path]
 				
@@ -90,11 +90,11 @@ module Utopia
 					links
 				end
 			end
-
+			
 			def related_links
 				@controller.links(@uri_path.dirname, :name => @uri_path.basename, :indices => true)
 			end
-
+			
 			def siblings_path
 				if @uri_path.basename == INDEX
 					@uri_path.dirname(2)
@@ -102,7 +102,7 @@ module Utopia
 					@uri_path.dirname
 				end
 			end
-
+			
 			def sibling_links(**options)
 				return @controller.links(siblings_path, **options)
 			end
@@ -129,7 +129,7 @@ module Utopia
 			def process!(request, attributes = {})
 				Document.render(self, request, attributes).to_a
 			end
-		
+			
 			# This is a special context in which a limited set of well defined methods are exposed in the content view.
 			Context = Struct.new(:document, :state) do
 				def partial(*args, &block)
@@ -173,11 +173,11 @@ module Utopia
 				def content
 					document.content
 				end
-
+				
 				def parent
 					document.parent
 				end
-
+				
 				def first
 					document.first
 				end
